0557440d8249776e92d9edfd166f6ff48cae19ba
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-06-03 18:57:04 +0300

    chore: code comments added

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,8 @@
 import TodoForm from "./TodoForm.tsx";
-import {useReducer} from "react";
+import { useReducer } from "react";
 import TodoList from "./TodoList.tsx";
 
+// elements we want each to-do element to include, text and an id to add/delete it by
 type TodoProps = {
     id: number;
     text:string;
@@ -18,15 +19,18 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                     id: Date.now(),
             text: action.payload
         };
-            return [...state, newTodo];
+            return [...state, newTodo]; // copies the current state array and adds a newTodo
+                                        // array-spreading for immutability; doesn't modify existent array
         }
             case "DELETE":
+                    // returns a new array containing only todos whose id isn't the same as the action payload one
                     return state.filter(todo => todo.id !== action.payload);
+            default: return state;
     }
-
 }
 
-const Todo = () =>{
+// also creates a specific kind of comment
+const Todo = () => {
 
     const [todos, dispatch] = useReducer(todoReducer, []);
     // const [todos, setTodos] = useState([]); // με useState
